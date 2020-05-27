cafa43af3b6c80a398507151d334b0f017641a6a
ElgizCebrayilov992 <elgizcebrayilov1992@gmail.com>
2020-05-28 02:54:34 +0400

    Resume Swing App

---
+++ ResumeDesktopApp/src/main/java/com/mycompany/resume/config/Config.java
@@ -0,0 +1,18 @@
+package com.mycompany.resume.config;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import com.mycompany.entity.User;
+
+/**
+ *
+ * @author virtu
+ */
+public class Config {
+    public static User loggedInUser;
+}
